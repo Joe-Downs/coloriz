ec3a60db24188b468d2a3afb6c7bf6e9f2e4c68f
Joe Downs <joedowns502@gmail.com>
2021-01-17 23:23:36 -0500

    coloriz.py: (1/2) Move color-related functions to separate module
    For brevity in the main bot file, coloriz.py, any and all functions
    relating to creating or deleting the color roles were moved to a
    seperate module file, colorCommands.py.

--- coloriz.py
+++ coloriz.py
@@ -14,61 +14,6 @@ intents.members = True
 
 bot = commands.Bot(command_prefix = prefix, intents = intents)
 
-# Returns the index position of the Bots top role in the guild's list of roles
-def getBotTopRoleNum(ctx):
-    botMember = ctx.guild.me
-    botRoles = botMember.roles
-    return len(botRoles) - 1
-    
-async def assignColor(ctx, red, green, blue):
-    color = discord.Color.from_rgb(int(red), int(green), int(blue))
-    user = ctx.message.author
-    topRole = user.top_role
-    for role in user.roles:
-        if str(role).startswith("#"):
-            await user.remove_roles(role)
-            break
-    needsNewRole = True
-    for role in ctx.guild.roles:
-        if role.name == str(color):
-            print(f"{role.name} is {str(color)}")
-            await user.add_roles(role)
-            needsNewRole = False
-            break
-        
-    if needsNewRole:
-        role = await ctx.guild.create_role(name = str(color),
-                                           color = color)
-        await user.add_roles(role)
-        # The top color role will be the one directly under the bot's own top role
-        topColorRoleNum = getBotTopRoleNum(ctx) - 1
-        await ctx.guild.edit_role_positions(positions = {role: topColorRoleNum})
-    return color
-
-async def cleanupColors(ctx):
-    rolesDeleted = 0
-    for role in ctx.guild.roles:
-        if str(role).startswith("#"):
-            if len(role.members) == 0:
-                await role.delete(reason = "Unused")
-                rolesDeleted += 1
-    message = f"Deleted {rolesDeleted} roles."
-    await ctx.send(message)
-
-def countColorRoles(ctx):
-    roleCount = 0
-    for role in ctx.guild.roles:
-        if str(role).startswith("#"):
-            roleCount += 1
-    return roleCount
-
-async def removeColorRole(ctx):
-    user = ctx.message.author
-    for role in user.roles:
-        if str(role).startswith("#"):
-            await user.remove_roles(role)
-            break
-
 colorHelpMessage = "Choose your own color role with this command, replacing R G B with an RGB triplet. \n"
 colorHelpMessage += "Replace the triplet with \"clear\" to clear your color role."
         
@@ -156,3 +101,30 @@ async def sudo(ctx, arg):
         await cleanupColors(ctx)
 
 bot.run(botToken)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
