049da1b61db280ad11a963aba1feebc691c3f738
Joe Downs <joedowns502@gmail.com>
2021-02-25 00:08:31 -0500

    botCommands.py: simplify hex conversion function
    The function now returns three separate red, green, and blue values
    rather than returning one tuple of said values. Additionally, the
    conversions to RGB were combined to one line for each integer
    
    ~ hexToRGB()

--- botCommands.py
+++ botCommands.py
@@ -1,6 +1,6 @@
 # All commands (and their necessary functions) for the coloriz bot
 
-# Returns a tuple of RGB values from a given hex color value
+# Returns a three values (red, green, blue) from a given hex color value
 def hexToRGB(hexValue):
     # If the hexValue string isn't 7 characters, it's not valid
     if len(hexValue) != 7:
@@ -8,15 +8,7 @@ def hexToRGB(hexValue):
     # If the hexValue string doesn't start with a '#', it's not valid
     if not hexValue.startswith('#'):
         raise ValueError(f"{hexValue} does not begin with #")
-    red = hexValue[1]
-    red += hexValue[2]
-    red = int(red, 16)
-    green = hexValue[3]
-    green += hexValue[4]
-    green = int(green, 16)
-    blue = hexValue[5]
-    blue += hexValue[6]
-    blue = int(blue, 16)
-    return (red, green, blue)
-
-
+    red = int(hexValue[1] + hexValue[2], 16)
+    green = int(hexValue[3] + hexValue[4], 16)
+    blue = int(hexValue[5] + hexValue[6], 16)
+    return red, green, blue
